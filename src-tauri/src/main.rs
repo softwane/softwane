e3f66637b7bb71dcb060d5932e39f7e7c08e7c52
Anchor4P <131856714+perthonBalans@users.noreply.github.com>
2026-05-07 17:33:29 +0800

    feat: use mimalloc as the global allocator

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,10 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 fn main() {
     erode_app_lib::run();
 }
